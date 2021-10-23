5d1d145c0447feb3e5b27f14b141ad8c692445be
Tim <timrus99@gmail.com>
2021-10-23 10:50:24 +0200

    Colours & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('First App'),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
 ),
   body: Center(
-          child: Text('Hello user!'),
+    child: Text(
+      'Hello user!',
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 2.0,
+        color: Colors.grey[600],
+        fontFamily: 'SansPro',
+      ),
+    ),
   ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {  },
       child: Text('CLICK'),
+      backgroundColor: Colors.red[600],
     ),
   ),
 ));
